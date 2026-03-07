77ef25770f8b94069ba6de324d7d0dfcd39800ac
Dmitriy <babygreenyoda@yandex.ru>
2026-03-07 14:09:57 +0800

    Added function forming array and def union

--- cv/m3.py
+++ cv/m3.py
@@ -3,8 +3,22 @@ import numpy as np
 
 def neighbours(y,x):
     return (y-1,x),(y,x-1)
+
+def find(label, links):
+    j = label
+    while links[j] !=0:
+        j = links[j]
+    return j 
+
+def union(label1,label2,links):
+    j = find(label1,links)
+    k = find(label2,links)
+    if j != k:
+        links[k] = j
+
 def label(binary):
     labeled = binary * -1
+    links = np.zeros(labeled.size//2 +1, dtype ='int32')
     label = 0
     for y in range(1,binary.shape[0]-[1]):
         for x in range(1,binary.shape[1]-[1]):
@@ -16,8 +30,11 @@ def label(binary):
                     label +=1
                     lb = label
                 elif n1 ==0 or n2 ==0:
-                    lh =max([n1,n2])
+                    lb =max([n1,n2])
                 else: 
                     lb = min([n1,n2])
-
+                labeled[y,x] = lb
+                union(n1,lb,links)
+                union(n1,lb,links)
+    print(links[:10])
     return labeled
